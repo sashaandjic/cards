c19cf7282be9ef0e2211b9d60afda50da0cad069
veronikaslc <veronika.koltunova@sickkids.ca>
2022-08-09 03:38:47 -0400

    [misc] Fixed console error regarding missing Required ref property

--- modules/data-entry/src/main/frontend/src/questionnaire/NumberQuestion.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/NumberQuestion.jsx
@@ -453,11 +453,8 @@ function NumberQuestion(props) {
 }
 
 // Helper function to bridge react-number-format with @material-ui
-export const NumberFormatCustom = forwardRef(function NumberFormatCustom(
-    props,
-    ref,
-  ) {
-  const { inputRef, onChange, ...other } = props;
+export const NumberFormatCustom = forwardRef((props, ref) => {
+  const { onChange, ...other } = props;
 
   return (
     <NumberFormat
@@ -475,7 +472,6 @@ export const NumberFormatCustom = forwardRef(function NumberFormatCustom(
 });
 
 NumberFormatCustom.propTypes = {
-  ref: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
