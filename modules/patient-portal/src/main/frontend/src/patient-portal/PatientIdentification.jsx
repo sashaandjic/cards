d3d0d887c6a0342bed8683d4f8c504cc9e3c8fbc
veronikaslc <veronika.koltunov@gmail.com>
2022-08-18 23:01:29 -0400

    CARDS-1761: Adapt React Date / Time pickers Removed all unused props from date time pickers components

--- modules/patient-portal/src/main/frontend/src/patient-portal/PatientIdentification.jsx
+++ modules/patient-portal/src/main/frontend/src/patient-portal/PatientIdentification.jsx
@@ -368,7 +368,6 @@ function PatientIdentification(props) {
                     }}
                   />
                 }
-                showToolbar
               />
             </LocalizationProvider>
             <Grid container direction="row" alignItems="flex-end" spacing={3} wrap="nowrap" justifyContent="space-between" className={classes.identifierContainer}>
